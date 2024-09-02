b799992b387bfb84a2204b41fc03492171b78c2b
Murilo Thom <murilothom.2002@hotmail.com>
2024-09-01 22:12:33 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -20,9 +20,13 @@ async function bootstrap() {
   const envService = app.get(EnvService);
   const port = envService.get('PORT');
 
+  const envMode = envService.get('NODE_ENV');
+
+  const origin =
+    envMode === 'production' ? 'https://coderace-web.vercel.app' : '*';
   app.enableCors({
-    origin: '*',
-    methods: 'GET,POST,PATCH,PUT,DELETE',
+    origin,
+    methods: 'GET,POST,PATCH,PUT,DELETE,OPTIONS',
     allowedHeaders: '*',
     credentials: true,
   });
